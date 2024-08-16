f1386790119973b953a2a23fbeaefac00c867cdb
JhoniPS <jhonisilva500@gmail.com>
2024-08-16 00:10:34 -0300

    Change method store -> UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,28 +21,27 @@ class UserController extends Controller
     {
         $request->validate([
             'file' => ['required', 'file', 'mimes:csv,txt', 'max:2048'],
-        ],[
-            'file.required' => 'O campo file é obrigatorio.',
+        ], [
+            'file.required' => 'O campo file é obrigatório.',
             'file.mimes' => 'O campo deve ter um formato CSV ou TXT.',
-            'file.max' => 'Tamanho do arquivo execede :max Mb.',
+            'file.max' => 'O tamanho do arquivo excede :max KB.',
         ]);
 
         $headers = ['name', 'email', 'password'];
-
         $dataFile = array_map('str_getcsv', file($request->file('file')));
 
         $arrayValues = [];
         $numberRegisterRecords = 0;
-        $emailReadyRegistered = false;
+        $emailReadyRegistered = [];
 
         foreach ($dataFile as $keyData => $valueData) {
-            $values = explode(";", $valueData[0]);
+            $values = str_getcsv($valueData[0], ';');
             $userData = [];
 
             foreach ($headers as $keyHeader => $header) {
-                if($header === 'email') {
-                    if(User::where('email', $values[$keyHeader])->first()) {
-                        $emailReadyRegistered .= $values[$keyHeader] . ", ";
+                if ($header === 'email') {
+                    if (User::where('email', $values[$keyHeader])->exists()) {
+                        $emailReadyRegistered[] = $values[$keyHeader];
                     }
                 }
 
@@ -54,18 +53,18 @@ class UserController extends Controller
             }
 
             $numberRegisterRecords++;
-
             $arrayValues[] = $userData;
         }
 
-        if($emailReadyRegistered) {
-            return back()->with('error', 'Dados não cadastrados.' . ' Existem e-mails já cadastrados: '. $emailReadyRegistered);
+        if (!empty($emailReadyRegistered)) {
+            return back()->with('error', 'Dados não cadastrados. E-mails já cadastrados: ' . implode(', ', $emailReadyRegistered));
         }
 
         User::insert($arrayValues);
 
         return back()->with('success', 'Dados inseridos com sucesso! Quantidade: ' . $numberRegisterRecords);
     }
+
     public function destroy(string $id): RedirectResponse
     {
         if(!$user = User::find($id)) {
